28f804473795ebf7f3d6ab9f1e41b7d63637d5b3
emilgelm <emil.gelman@gmail.com>
2020-03-15 21:33:47 +0200

    add green splash screen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,9 +5,9 @@ import {Icon} from "expo";
 import {Video} from "expo-av";
 
 export default class HomeScreen extends Component {
-state = {
-    value: 5
-};
+    state = {
+        value: 5
+    };
 
     constructor(props) {
         super(props);
@@ -16,51 +16,50 @@ state = {
     render() {
         return (
             <View style={styles.container}>
+                <Video
+                    source={require('../assets/corona.mp4')}
+                    rate={1.0}
+                    volume={1.0}
+                    isMuted={false}
+                    resizeMode="cover"
+                    shouldPlay
+                    isLooping
+                    style={styles.backgroundVideo}
 
-               <Video
-                   source={require('../assets/corona.mp4')}
-                   rate={1.0}
-                   volume={1.0}
-                   isMuted={false}
-                   resizeMode="cover"
-                   shouldPlay
-                   isLooping
-                   style={styles.backgroundVideo}
-
-               >
-               </Video>
-                   <View style={styles.loginButtonSection}>
-                       <Text style={styles.text}>רדיוס:</Text>
-                       <Slider
-                           style={{ width: 300 }}
-                           step={1}
-                           minimumValue={5}
-                           maximumValue={15}
-                           value={this.state.value}
-                           onValueChange={val => this.setState({ value: val })}
-                       />
-                       <Text style={styles.text}>{this.state.value}</Text>
-                       <Button
-                           title="חיפוש"
-                           onPress={() => this.props.navigation.navigate('Details', {radius: this.state.value})}
-                           titleStyle={{ fontWeight: 'bold', color: 'white', fontSize: 48 }}
-                           type="outline"
-                           buttonStyle={{
-                               borderWidth: 1,
-                               borderColor: 'white',
-                               borderRadius: 20,
-                           }}
-                           icon={{
-                               name: 'search',
-                               type: 'font-awesome',
-                               size: 48,
-                               color: 'white',
-                           }}
-                           iconRight
-                       />
-                   </View>
+                >
+                </Video>
+                <View style={styles.loginButtonSection}>
+                    <Text style={styles.text}>רדיוס:</Text>
+                    <Slider
+                        style={{width: 300}}
+                        step={1}
+                        minimumValue={5}
+                        maximumValue={15}
+                        value={this.state.value}
+                        onValueChange={val => this.setState({value: val})}
+                    />
+                    <Text style={styles.text}>{this.state.value}</Text>
+                    <Button
+                        title="חיפוש"
+                        onPress={() => this.props.navigation.navigate('Details', {radius: this.state.value})}
+                        titleStyle={{fontWeight: 'bold', color: 'white', fontSize: 48}}
+                        type="outline"
+                        buttonStyle={{
+                            borderWidth: 1,
+                            borderColor: 'white',
+                            borderRadius: 20,
+                        }}
+                        icon={{
+                            name: 'search',
+                            type: 'font-awesome',
+                            size: 48,
+                            color: 'white',
+                        }}
+                        iconRight
+                    />
+                </View>
             </View>
-    )
+        )
     }
 }
 
